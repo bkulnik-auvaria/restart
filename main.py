5d1e4dc4f99186d19a38b6e231fedd42c34bc454
Benjamin Kulnik <b.kulnik@auvaria.com>
2023-03-02 13:53:46 +0100

    Implemented base calculator

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("hello world!")
+
+# Enter first number: 2134
+# Enter second number: 2134
+# Enter the operation: +,-
+# result: first number + second number
+
+print("Enter first number: ", end="")
+x_input = input()
+x = int(x_input)
+
+
+print("Enter second number: ", end="")
+y_input = input()
+y = int(y_input)
+
+
+print("Enter operation: ", end="")
+operation = input()
+
+print("The operation is: ", operation)
+
+# = ---> assign the right side to the left side
+# == ---> check for equality
+if operation == "+":
+    result = x + y # All i write here, is totally ignored by python
+elif operation == "-":
+    result = x - y
+else:
+    print("Invalid operation")
+    exit(-1)
+
+print("The result: ", result)
